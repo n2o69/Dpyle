b33e8a6f26fff05a30600c19e4201bc89949609b
n2o <unmec951@gmail.com>
2026-05-23 02:11:23 +0200

    fixing "missing WorkFolder creation"

--- Install.cmd
+++ Install.cmd
@@ -1,5 +1,6 @@
 @echo off
 title Installing Dependencies for Dpyle
+mkdir WorkFolder
 pip install -r requirements.txt
 powershell -Command ^
 "Start-Process -FilePath 'pylingual-main/prepare_pylingual.cmd' -WorkingDirectory pylingual-main -Verb RunAs"
